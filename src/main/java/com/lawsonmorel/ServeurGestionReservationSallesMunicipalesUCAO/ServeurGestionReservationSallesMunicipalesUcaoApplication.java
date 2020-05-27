d90bf0132a19e69319c5bb871dabaf98d01a6e1b
LAWSON ANANISSO MOREL LATE <morellatel@gmail.com>
2020-05-27 19:11:22 +0000

    first commit

---
+++ src/main/java/com/lawsonmorel/ServeurGestionReservationSallesMunicipalesUCAO/ServeurGestionReservationSallesMunicipalesUcaoApplication.java
@@ -0,0 +1,13 @@
+package com.lawsonmorel.ServeurGestionReservationSallesMunicipalesUCAO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionReservationSallesMunicipalesUcaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionReservationSallesMunicipalesUcaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lawsonmorel/ServeurGestionReservationSallesMunicipalesUCAO/ServeurGestionReservationSallesMunicipalesUcaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawsonmorel.ServeurGestionReservationSallesMunicipalesUCAO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionReservationSallesMunicipalesUcaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
